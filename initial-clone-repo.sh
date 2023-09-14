2e2b2f44056dbf2da6bcf4423c3fb3a1e90f52cd
Carsten Lackner <carsten.lackner@tu-berlin.de>
2023-09-14 10:43:27 +0200

    major changes to pmlmodelling NEMO-container add fabm and ersem ;update to Nemo 4.20 and xios 2.5 with changed flags and file updates according to J Mak

--- initial-clone-repo.sh
+++ initial-clone-repo.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/bash
 
-git clone git@gitlab.ecosystem-modelling.pml.ac.uk:gle/nemo404.git nemo
-svn checkout http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/branchs/xios-2.5 xios
-zip -r nemo-xios.zip nemo xios
+#git clone git@gitlab.ecosystem-modelling.pml.ac.uk:gle/nemo404.git nemo
+svn co http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS2/trunk xios2
+zip -r xios2.zip xios2
+rm -rf xios2
+
+# git clone --branch 4.2.0 https://forge.nemo-ocean.eu/nemo/nemo.git nemo
+# zip -r nemo_420.zip nemo
+# rm -rf nemo
---
+++ oldREADME.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# CoRNErS
+Containerisation of Revised NEMO-ERSEM Employing Singularity (CoRNErS)
+=======
+# NEMO-container
+NEMO-ERSEM container
+>>>>>>> 82f506b (Initial commit)
