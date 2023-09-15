d3665763b216a9e3d49d178786094f5421d04830
Carsten Lackner <carsten.lackner@tu-berlin.de>
2023-09-15 18:09:54 +0200

    gotm.def painstaking errors with netcdf... still not working properly

--- initial-clone-repo.sh
+++ initial-clone-repo.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/bash
 
-#git clone git@gitlab.ecosystem-modelling.pml.ac.uk:gle/nemo404.git nemo
+### old nemo
+# git clone git@gitlab.ecosystem-modelling.pml.ac.uk:gle/nemo404.git nemo
+
+### xios rewritten to get new version
+### also divided nemo and xios .zip files
 svn co http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS2/trunk xios2
 zip -r xios2.zip xios2
 rm -rf xios2
 
-# git clone --branch 4.2.0 https://forge.nemo-ocean.eu/nemo/nemo.git nemo
-# zip -r nemo_420.zip nemo
-# rm -rf nemo
+git clone --branch 4.2.0 https://forge.nemo-ocean.eu/nemo/nemo.git nemo
+zip -r nemo_420.zip nemo
+rm -rf nemo
